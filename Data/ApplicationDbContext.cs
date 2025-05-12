c2b2c083c8109c5bf8948714eb73379d61b3de85
Naz <171716584+nazbur05@users.noreply.github.com>
2025-05-12 18:04:50 +0200

    add mailkit and identity dependencies, set up the folder structure

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+//I will add here in the future
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
