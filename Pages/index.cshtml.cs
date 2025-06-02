55793685fecb3a52c55ae86ea23f6639d9d571fd
Gulzat <gulzatyessen@gmail.com>
2025-06-02 16:10:26 +0300

    fix cshtml

---
+++ Pages/index.cshtml.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace YourAppNamespace.Pages
+{
+    public class SignupModel : PageModel
+    {
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // TODO: Save user data to database here
+
+            return RedirectToPage("/Login");
+        }
+
+        public class InputModel
+        {
+            [Required(ErrorMessage = "Full name is required")]
+            [Display(Name = "Full Name")]
+            public string FullName { get; set; }
+
+            [Required(ErrorMessage = "Email is required")]
+            [EmailAddress(ErrorMessage = "Invalid email address")]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = "Password is required")]
+            [DataType(DataType.Password)]
+            [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
+            public string Password { get; set; }
+        }
+    }
+}
